0b768f056b6d6d36a15db6913130bc990bca9ee6
fannygabriela.becerra <fannygabriela.becerra@bbva.com>
2020-09-03 19:10:05 -0500

    Clase 42: Espress II

--- Challenge/Fanny_Gabriela_Becerra_Hernandez/challenge.js
+++ Challenge/Fanny_Gabriela_Becerra_Hernandez/challenge.js
@@ -3,21 +3,21 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('¡Hola mundo!')
 })
 
 app.post('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('¡Hola mundo!')
 })
 
 app.put('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('¡Hola mundo!')
 })
 
 app.delete('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('¡Hola mundo!')
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Escuchando en el puerto http://localhost:${port}`)
 })
